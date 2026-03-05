0c8dcc2c9d11d40a583b40b6805f5b105af50b40
Rohith <rohithkumarcm026@gmail.com>
2026-03-05 15:33:35 +0530

    final_project commit

--- tests/test_arna.py
+++ tests/test_arna.py
@@ -1,11 +1,7 @@
-# from src.ui_telegram import main
-# import threading
-# def test_main():
-#     threading.Thread(target=main).start()
-#     assert main() f
-import tensorflow as tf
+from src.ui_telegram import main
+import threading
 
 
 def test_model_loading():
-    model = tf.keras.models.load_model("Models/best_model.keras")
-    assert model is not None
+    threading.Thread(target=main).start()
+    assert main() 
