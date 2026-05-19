7b12b95b10b4710350eb6f0ed42458743ecac4c0
Rohith <rohithkumarcm026@gmail.com>
2026-05-19 14:28:32 +0530

    models_path_added

--- Backend/src/fertilizer_recomendation.py
+++ Backend/src/fertilizer_recomendation.py
@@ -12,16 +12,26 @@ def recommend_fertilizer(disease):
     BASE_DIR = Path(__file__).resolve().parent
 
     MODEL_DIR = BASE_DIR / "Models"
+    DATASET_DIR = BASE_DIR / "Dataset"
 
     fertilizer_model_path = MODEL_DIR / "Recomendation_chatbot_model.pkl"
     tokenizer_path = MODEL_DIR / "recomendation_vectorizer.pkl"
+    json_path = DATASET_DIR / "Fartillizer_recom.json"
     try:
+        # Load model
         fertilizer_model = joblib.load(fertilizer_model_path)
-        with open('Dataset/Fartillizer_recom.json', 'r') as f:
+
+        # Load tokenizer
+        with open(tokenizer_path, "rb") as f:
+            tokenizer = pickle.load(f)
+
+        # Load intents
+        with open(json_path, "r") as f:
             intents = json.load(f)
-        tokenizer = pickle.load(tokenizer_path)
+
     except Exception as e:
-        print("model not found",e)
+        print("Model loading error:", e)
+        return "Fertilizer recommendation unavailable"
     # Tokenize the disease index or name (modify based on tokenizer format)
     if disease=="Normal":
       return "Your Crop is Healthy No Need Fertilizers"
